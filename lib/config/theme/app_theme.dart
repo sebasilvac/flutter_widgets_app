dda1898a19b843bf3a8beb90f6cc3137eeb64c3b
Sebastián Silva <sebasilvac88@gmail.com>
2023-02-22 15:29:55 -0300

    Theme + MenuItems

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'SelectedColor must be greater then zero'),
+        assert(selectedColor < colorList.length,
+            'SelectedColor must be between 0 and ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+        ),
+      );
+}
