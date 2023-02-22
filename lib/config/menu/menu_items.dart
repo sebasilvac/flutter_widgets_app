107f54cfbfb7acb745742fe71c11cb63f5ab5c6c
Sebastián Silva <sebasilvac88@gmail.com>
2023-02-22 18:34:23 -0300

    configured basic routes

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,7 +24,7 @@ const appMenuItems = <MenuItems>[
   MenuItems(
     title: 'Tarjetas',
     subTitle: 'Contenedor estilizado',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card,
   ),
 ];
