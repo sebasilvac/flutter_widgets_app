56d18730fe35d391c52bd4235693c2b6c9c8ff57
Sebastián Silva <sebasilvac88@gmail.com>
2023-02-23 01:22:06 -0300

    routes with names

--- lib/presentations/screens/home_screen.dart
+++ lib/presentations/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentations/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -61,7 +63,8 @@ class _CustomListTile extends StatelessWidget {
 
         // Navigator.pushNamed(context, menuItem.link);
 
-        context.push(menuItem.link);
+        //context.push(menuItem.link);
+        context.pushNamed(CardsScreen.name);
       },
     );
   }
